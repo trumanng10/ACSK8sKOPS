9af6daab9f1248a7510b2525b4150f19ed34f9dd
trumanng10 <35524030+trumanng10@users.noreply.github.com>
2022-08-18 08:27:46 +0800

    Add files via upload

---
+++ 003_installKOPS.sh
@@ -0,0 +1,9 @@
+curl -LO https://github.com/kubernetes/kops/releases/download/v1.21.0/kops-linux-amd64
+
+chmod +x kops-linux-amd64
+
+sudo mv kops-linux-amd64 /usr/local/bin/kops
+
+sudo apt install -y python-pip
+sudo pip install awscli
+
