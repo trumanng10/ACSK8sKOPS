88f030d2cf6c4536e4f5d14b174218d3243d21e8
trumanng10 <35524030+trumanng10@users.noreply.github.com>
2022-08-18 08:28:39 +0800

    Delete 003_installKOPS.sh

--- 003_installKOPS.sh
+++
@@ -1,9 +0,0 @@
-curl -LO https://github.com/kubernetes/kops/releases/download/v1.21.0/kops-linux-amd64
-
-chmod +x kops-linux-amd64
-
-sudo mv kops-linux-amd64 /usr/local/bin/kops
-
-sudo apt install -y python-pip
-sudo pip install awscli
-
